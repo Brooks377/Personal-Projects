b0d9363b253838688110de67ccee213b4c0d765e
Brooks Walsh <lbw223@lehigh.edu>
2023-03-15 22:52:47 -0400

    update 3/15

--- Amortization Table/validate_date.py
+++ Amortization Table/validate_date.py
@@ -1,12 +1,11 @@
 # date validation function for use in amortization table
 from datetime import datetime
-def validate_date(start_date, end_date):
-    if isinstance(start_date, datetime) and isinstance(end_date, datetime):
+def validate_date(start_date):
+    if isinstance(start_date, datetime):
         return True
-    elif isinstance(start_date, str) and isinstance(end_date, str):
+    elif isinstance(start_date, str):
         try:
             datetime.strptime(start_date, '%Y-%m-%d')
-            datetime.strptime(end_date, '%Y-%m-%d')
             return True
         except ValueError:
             return False
