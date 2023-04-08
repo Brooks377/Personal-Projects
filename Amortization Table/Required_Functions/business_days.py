ddd37d0deb261594e031a4c1f8a3cefc3f7b361c
Brooks Walsh <lbw223@lehigh.edu>
2023-04-08 00:32:14 -0400

    added to x-axis and title

--- Amortization Table/Required_Functions/business_days.py
+++ Amortization Table/Required_Functions/business_days.py
@@ -5,7 +5,7 @@ def business_days(start_date, end_date):
     delta = timedelta(days=1)
     while start_date <= end_date:
         if start_date.weekday() not in [5, 6]: # check if the date is not a Saturday or Sunday
-            dates.append(start_date)
+            dates.append(start_date.strftime('%Y-%m-%d'))
         start_date += delta
 
     return dates
